881a6117835c4cd5d1d6aa806d178aaac38b2923
Nikoto Koto <floris.nicolas77@gmail.com>
2025-09-24 18:19:08 +0200

    🎉  Begin football api project

---
+++ src/test/java/com/example/football_team_api/FootballTeamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.football_team_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
